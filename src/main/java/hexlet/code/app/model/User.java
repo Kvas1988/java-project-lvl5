6b70126b73ba7a7cdfcda747d681886b4e057791
Kvas <shokhin.al@gmail.com>
2022-02-24 11:39:03 +0300

    users rename tablename

--- src/main/java/hexlet/code/app/model/User.java
+++ src/main/java/hexlet/code/app/model/User.java
@@ -7,11 +7,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
