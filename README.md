458bb0ac48684bcf2111452a759b9e216be4ceac
Kvas <shokhin.al@gmail.com>
2022-05-06 01:33:47 +0300

    Task 11. Rollback setup

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/b6c9bdc687ac8c193d51/maintainability)](https://codeclimate.com/github/Kvas1988/java-project-lvl5/maintainability)  
 [![Test Coverage](https://api.codeclimate.com/v1/badges/b6c9bdc687ac8c193d51/test_coverage)](https://codeclimate.com/github/Kvas1988/java-project-lvl5/test_coverage)  
 
+
+[Swagger API Docs (Heroku)](https://hexlet-project5.herokuapp.com/swagger-ui/index.html)
---
+++ src/main/java/hexlet/code/app/config/RollbarConfig.java
@@ -0,0 +1,42 @@
+package hexlet.code.app.config;
+
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@Configuration()
+@EnableWebMvc
+@ComponentScan({
+        "hexlet.code.app.config",
+        "com.rollbar.spring"
+})
+public class RollbarConfig {
+
+    @Value("${rollbar_token}")
+    String token;
+
+    @Value("${SPRING_PROFILES_ACTIVE:dev}")
+    String env;
+
+    /**
+     * Register a Rollbar bean to configure App with Rollbar.
+     */
+    @Bean
+    public Rollbar rollbar() {
+        return new Rollbar(getRollbarConfigs(token));
+    }
+
+    private Config getRollbarConfigs(String accessToken) {
+
+        // Reference ConfigBuilder.java for all the properties you can set for Rollbar
+        return RollbarSpringConfigBuilder.withAccessToken(accessToken)
+                .environment(env)
+                .build();
+    }
+}
