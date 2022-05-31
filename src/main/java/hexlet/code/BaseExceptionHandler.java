43b3f8643da1a62e544d25f441a04122b029e829
Kvas <shokhin.al@gmail.com>
2022-05-31 12:33:16 +0300

    Main Page 404 fixed

--- src/main/java/hexlet/code/BaseExceptionHandler.java
+++ src/main/java/hexlet/code/BaseExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.validation.ConstraintViolationException;
 import java.util.NoSuchElementException;
 
 @ResponseBody
